e77e2a7cad3a8ad9387b86484f3ef99b62327659
hahaahhaa <pjw319@naver.com>
2025-03-23 21:38:12 +0900

    애니메이션 작업, 마무리

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const contents = document.querySelectorAll('.total_section');
+const contents1 = document.querySelectorAll('.semi_section');
+const contents2 = document.querySelectorAll('.main_title');
 
 function animationWhendisplayed() {
   window.addEventListener('scroll', () => {
@@ -10,4 +12,26 @@ function animationWhendisplayed() {
   })
 }
 
+function animationWhendisplayed1() {
+  window.addEventListener('scroll', () => {
+    contents1.forEach((content) => {
+      if(window.innerHeight > content.getBoundingClientRect().top){
+        content.classList.add('on');
+      }
+    })
+  })
+}
+
+function animationWhendisplayed2() {
+  window.addEventListener('scroll', () => {
+    contents2.forEach((content) => {
+      if(window.innerHeight > content.getBoundingClientRect().top){
+        content.classList.add('on');
+      }
+    })
+  })
+}
+
 animationWhendisplayed();
+animationWhendisplayed1();
+animationWhendisplayed2();
